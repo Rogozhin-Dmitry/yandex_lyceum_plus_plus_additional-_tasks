ee835dea13f2900ca96b9e75361a034b84d1b108
Rogozhin-Dmitry <62459927+Rogozhin-Dmitry@users.noreply.github.com>
2022-03-19 19:02:15 +0300

    Second lesson (error correction)

--- lyceum11/catalog/urls.py
+++ lyceum11/catalog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import *
+from .views import item_list, item_detail
 
 urlpatterns = [
     url(r'^$', item_list),
--- lyceum11/users/urls.py
+++ lyceum11/users/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from .views import *
+from .views import user_list, user_detail, signup, profile
 
 urlpatterns = [
     url(r'^users/$', user_list),
     url(r'^users/([0-9]+)/$', user_detail),
-    url(r'^signup/', signup),
-    url(r'^profile/', profile),
+    url(r'^signup/$', signup),
+    url(r'^profile/$', profile),
 ]
